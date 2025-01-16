0556bf3ff6bf12a29a9acec3410ed38bf781406e
suraj-markup <sk9261712674@gmail.com>
2025-01-16 18:54:56 +0530

    updated origin

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,4 +1,4 @@
 export const AVATAR_API = "https://ui-avatars.com/api";
-export const API = "http://localhost:1337/api";
+export const API = "https://prized-nature-98a1af9371.strapiapp.com/";
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
